2c5691f618af6cd8fdc1a274f4dd8d51de6b2e6d
juniobc <juniobc@gmail.com>
2015-11-30 22:08:18 -0200

    Criando a tela de cadastro de produto

--- src/com/econoom/Home.java
+++ src/com/econoom/Home.java
@@ -1,16 +1,27 @@
 package com.econoom;
 
-import android.app.Activity;
-import android.os.Bundle;
+import android.app.*;
+import android.content.*;
+import android.os.*;
+import android.view.*;
 
 public class Home extends Activity {
 	
+	public static final String TAG = "Home";
+	
 	@Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.home);
         
     }
+	
+	public void cadastraProduto(View v){
+		//Log.d(TAG,"cadastraProduto");
+		Intent i = new Intent(Home.this, CadastroProduto.class);
+		startActivity(i);
+		
+	}
 
 }
