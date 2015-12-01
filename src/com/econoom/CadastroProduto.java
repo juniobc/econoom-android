c7bb71cc783b4a408daa6364a935ae552b50f19b
Sebastiao Junio <juniobc@gmail.com>
2015-12-01 18:09:57 -0200

    criando o spinner tipo de cadastro

--- src/com/econoom/CadastroProduto.java
+++ src/com/econoom/CadastroProduto.java
@@ -2,9 +2,13 @@ package com.econoom;
 
 import android.app.*;
 import android.os.*;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class CadastroProduto extends Activity
 {
+	
+	Spinner tp_cad;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -13,6 +17,14 @@ public class CadastroProduto extends Activity
 		super.onCreate(savedInstanceState);
 		
 		setContentView(R.layout.cadastro_produto);
+		
+		tp_cad = (Spinner) findViewById(R.id.tp_cad);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+		        R.array.tp_cad, android.R.layout.simple_spinner_item);
+		
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		
+		tp_cad.setAdapter(adapter);
 		
 	}
 	
