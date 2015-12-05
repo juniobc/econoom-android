840affd91e9bbeb400f090194d3fe6592290dad1
Sebastiao Junio <juniobc@gmail.com>
2015-12-05 12:54:51 -0200

    implementando a busca do endereco no google maps

--- src/com/econoom/MapaEndereco.java
+++ src/com/econoom/MapaEndereco.java
@@ -1,67 +1,76 @@
 package com.econoom;
 
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
 import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
 import com.econoom.localizacao.UltimoLocal;
 
-public class MapaEndereco extends FragmentActivity implements OnMapReadyCallback, OnMapLongClickListener {
+public class MapaEndereco extends FragmentActivity implements OnMapReadyCallback, OnMapLongClickListener{
 	
-	private GoogleMap map;
+	private static GoogleMap map;
 	private UltimoLocal ultimoLocal;
-	private GoogleApiClient mGoogleApiClient;
+	private static final String TAG = "MapaEndereco";
+	private Marker marker;
 
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        Log.d(TAG, "onCreate");
+        
         setContentView(R.layout.maps);
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);    
         
-        ultimoLocal = new UltimoLocal();
-        
-        buildGoogleApiClient();
+        ultimoLocal = new UltimoLocal(this);
                 
-    }
+    }	
 	
-	protected synchronized void buildGoogleApiClient() {
-	    mGoogleApiClient = new GoogleApiClient.Builder(this)
-	        .addConnectionCallbacks(ultimoLocal)
-	        .addOnConnectionFailedListener(ultimoLocal)
-	        .addApi(LocationServices.API)
-	        .build();
-	}
 
     @Override
-    public void onMapReady(GoogleMap map) {
+    public void onMapReady(GoogleMap gMap) {
     	
-    	this.map = map;
+    	Log.d(TAG, "onMapReady");
     	
-    	this.map.setOnMapLongClickListener(this);
+    	map = gMap;
     	
-    	this.map.setMyLocationEnabled(true);
-        //LatLng ultimolocal = new LatLng(local.getmLastLocation().getLatitude(), local.getmLastLocation().getLongitude());
-        //this.map.moveCamera(CameraUpdateFactory.newLatLngZoom(ultimolocal,10));
+    	map.setOnMapLongClickListener(this);
+    	
+    	map.setMyLocationEnabled(true);
+    	
+    	ultimoLocal.mGoogleApiClient.connect();    	
+        
     }
 
 	@Override
 	public void onMapLongClick(LatLng local) {
 		
-		this.map.addMarker(new MarkerOptions()
-        .position(local)
-        .title("Hello world"));
+		Log.d(TAG, "onMapLongClick");
+		
+		if(marker != null)
+			marker.remove();
+		
+		marker = map.addMarker(new MarkerOptions().position(local));
+		
+	}
+
+	public static void setMapaLocal(Location location) {
+		
+		Log.d(TAG, "onMapLoaded");
+
+		LatLng ultimolocal = new LatLng(location.getLatitude(), location.getLongitude());
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(ultimolocal,15));
 		
 	}
 
--- src/com/econoom/localizacao/UltimoLocal.java
+++ src/com/econoom/localizacao/UltimoLocal.java
@@ -1,6 +1,5 @@
 package com.econoom.localizacao;
 
-import android.app.Activity;
 import android.content.Context;
 import android.location.Location;
 import android.os.Bundle;
@@ -11,15 +10,29 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 import com.google.android.gms.location.LocationServices;
+import com.econoom.MapaEndereco;
 
-public class UltimoLocal implements ConnectionCallbacks, OnConnectionFailedListener {
+public final class UltimoLocal implements ConnectionCallbacks, OnConnectionFailedListener {
 
 	private Location mLastLocation;
-	private GoogleApiClient mGoogleApiClient;
+	public GoogleApiClient mGoogleApiClient;
 	private final String TAG = "UltimoLocal";
 	
-	public UltimoLocal(){
+	public UltimoLocal(Context contexto){
 		
+		buildGoogleApiClient(contexto);
+		
+	}
+	
+	protected synchronized void buildGoogleApiClient(Context contexto) {
+			
+			Log.d(TAG, "buildGoogleApiClient");
+			
+		    mGoogleApiClient = new GoogleApiClient.Builder(contexto)
+		        .addConnectionCallbacks(this)
+		        .addOnConnectionFailedListener(this)
+		        .addApi(LocationServices.API)
+		        .build();
 	}
 	
 	@Override
@@ -30,15 +43,20 @@ public class UltimoLocal implements ConnectionCallbacks, OnConnectionFailedListe
 
 	@Override
 	public void onConnected(Bundle arg0) {
-		
-		mLastLocation = LocationServices.FusedLocationApi.getLastLocation(this.mGoogleApiClient);
 		Log.d(TAG, "onConnected");
+		mLastLocation = LocationServices.FusedLocationApi.getLastLocation(this.mGoogleApiClient);
+		
+		Log.d(TAG, "latitude:"+mLastLocation.getLatitude());
+		Log.d(TAG, "longitude:"+mLastLocation.getLongitude());
+		
+		MapaEndereco.setMapaLocal(mLastLocation);
 		
 	}
 
 	@Override
 	public void onConnectionSuspended(int arg0) {
-		// TODO Auto-generated method stub
+
+		Log.d(TAG, "onConnectionFailed");
 		
 	}
 
